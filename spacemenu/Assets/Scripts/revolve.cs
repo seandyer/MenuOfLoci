e18671f357e1dd1abe30e4a0d66115e4c4f2b55e
nagabhavyam <nagabhavyam@gmail.com>
2015-11-29 16:34:34 -0500

    spacemenu

---
+++ spacemenu/Assets/Scripts/revolve.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class revolve : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.RotateAround(Vector3.zero,Vector3.up,0.1f);
+	
+	}
+}
